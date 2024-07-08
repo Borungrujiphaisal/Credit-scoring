644db735534596b844d0ffc8710463b647056429
Borungrujiphaisal <boonyachoke@hotmail.com>
2024-07-08 16:23:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,15 +8,15 @@ st.sidebar.header('Input Features')
 
 age                             = st.sidebar.number_input('Age', min_value=18, max_value=100, value=30)
 DSR                             = st.sidebar.number_input('Debt Service Ratio', min_value=0.0, value=0.3, step=0.01)
-MI                              = st.sidebar.number_input('Monthly Income', min_value=0.0, max_value=100000, value=15000.0, step=2500.0)
-NCLL                            = st.sidebar.number_input('Number of Open Credit Lines and Loans', min_value=0, max_value=50, value=5)
-WE                              = st.sidebar.number_input('Working experince (Months)', min_value=0, max_value=720, value=12)
+ME                              = st.sidebar.number_input('Monthly Expense', min_value=0.0, max_value=100000, value=15000.0, step=2500.0)
+ND                            = st.sidebar.number_input('Number of Dependents', min_value=0, max_value=10, value=3)
+WE                              = st.sidebar.number_input('Working experince (Months)', min_value=0, max_value=720, value=180)
 predict_button                  = st.sidebar.button('Predict')
 
 if predict_button:
   
-    value       = [age, DSR, MI, NCLL, WE]
-    coef        = [-1.05, -0.39, -0.56, 0.95, -0.102]
+    value       = [age, DSR, ME, ND, WE]
+    coef        = [-0.022, -1.02, 0.000056, 0.095, -0.0153]
     predicted_prob = 1/(1+np.exp(-np.dot(value,coef)))
     
     st.subheader('Estimate Probability of Default')
