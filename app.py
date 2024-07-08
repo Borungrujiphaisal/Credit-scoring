c98a1c5c5a000b8eeb96345b863fe1a21dfe7990
Borungrujiphaisal <boonyachoke@hotmail.com>
2024-07-08 16:10:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,71 +3,34 @@ import joblib
 import numpy as np
 import pandas as pd
 
-# Load the saved model
-model = joblib.load('combined_model.pkl')
-
-df = pd.read_csv('cs-training.csv')
-
-# Streamlit app UI
-st.title('Credit Risk Probability Calculator')
-
-# Add input widgets for user input
+st.title('Credit Score Calculator')
 st.sidebar.header('Input Features')
 
-RevolvingUtilizationOfUnsecuredLines = st.sidebar.number_input('Revolving Utilization of Unsecured Lines', min_value=0.0, max_value=1.0, value=0.5, step=0.01)
-age = st.sidebar.number_input('Age', min_value=18, max_value=100, value=30)
-NumberOfTime30_59DaysPastDueNotWorse = st.sidebar.number_input('Number of Times 30-59 Days Past Due Not Worse', min_value=0, max_value=20, value=0)
-DebtRatio = st.sidebar.number_input('Debt Ratio', min_value=0.0, value=0.3, step=0.01)
-MonthlyIncome = st.sidebar.number_input('Monthly Income', min_value=0.0, value=5000.0, step=100.0)
-NumberOfOpenCreditLinesAndLoans = st.sidebar.number_input('Number of Open Credit Lines and Loans', min_value=0, max_value=50, value=5)
-NumberOfTimes90DaysLate = st.sidebar.number_input('Number of Times 90 Days Late', min_value=0, max_value=20, value=0)
-NumberRealEstateLoansOrLines = st.sidebar.number_input('Number of Real Estate Loans or Lines', min_value=0, max_value=10, value=1)
-NumberOfTime60_89DaysPastDueNotWorse = st.sidebar.number_input('Number of Times 60-89 Days Past Due Not Worse', min_value=0, max_value=20, value=0)
-NumberOfDependents = st.sidebar.number_input('Number of Dependents', min_value=0, max_value=10, value=1)
+age                             = st.sidebar.number_input('Age', min_value=18, max_value=100, value=30)
+DSR                             = st.sidebar.number_input('Debt Service Ratio', min_value=0.0, value=0.3, step=0.01)
+MI                              = st.sidebar.number_input('Monthly Income', min_value=0.0, value=5000.0, step=100.0)
+NCLL                            = st.sidebar.number_input('Number of Open Credit Lines and Loans', min_value=0, max_value=50, value=5)
+WE                              = st.sidebar.number_input('Working experince (Months)', min_value=0, max_value=10, value=1)
+predict_button                  = st.sidebar.button('Predict')
 
-# Create a button to trigger prediction
-
-predict_button = st.sidebar.button('Predict')
-
-# Show input
-user_input = pd.DataFrame({
-    'Revolving Utilization of Unsecured Lines': [RevolvingUtilizationOfUnsecuredLines],
-    'Age': [age],
-    'Number of Times 30-59 Days Past Due Not Worse': [NumberOfTime30_59DaysPastDueNotWorse],
-    'Debt Ratio': [DebtRatio],
-    'Monthly Income': [MonthlyIncome],
-    'Number of Open Credit Lines and Loans': [NumberOfOpenCreditLinesAndLoans],
-    'Number of Times 90 Days Late': [NumberOfTimes90DaysLate],
-    'Number of Real Estate Loans or Lines': [NumberRealEstateLoansOrLines],
-    'Number of Times 60-89 Days Past Due Not Worse': [NumberOfTime60_89DaysPastDueNotWorse],
-    'Number of Dependents': [NumberOfDependents]
-},index = ['Value'])
-
-# Display the table of input values
-st.subheader('Input Values')
-st.table(user_input.T)
 
 # Display prediction result
 if predict_button:
-    user_input = np.array([
-        [RevolvingUtilizationOfUnsecuredLines, age, NumberOfTime30_59DaysPastDueNotWorse,
-        DebtRatio, MonthlyIncome, NumberOfOpenCreditLinesAndLoans, NumberOfTimes90DaysLate,
-        NumberRealEstateLoansOrLines, NumberOfTime60_89DaysPastDueNotWorse, NumberOfDependents]
-    ])
-
-  # Make prediction
-    predicted_prob = model.predict_proba(user_input)[0][1]
-    st.subheader('Prediction Probability')
+  
+    value       = [age, DSR, MI, NCLL, WE]
+    coef        = [-1.05, -0.39, -0.56, 0.95, -0.102]
+    predicted_prob = 1/(1+np.exp(np.dot(value,coef)))
+    
+    st.subheader('Estimate Probability of Default')
     st.write(f'Probability of Credit Risk: {predicted_prob:.4f}')
 
-    # Interpretation
     if predicted_prob >= 0.5:
-        st.write('Interpretation: Credit Risk')
+        st.write('Action: Reject')
+    elif 0.1 <= predicted_prob < 0.5:
+        st.write('Action: Refer')
     else:
-        st.write('Interpretation: No Credit Risk')
+        st.write('Action: Approve')
         
         
-# Run the Streamlit app
 if __name__ == '__main__':
-    st.sidebar.title('About')
-    st.sidebar.info('This app is a Credit Risk Probability Calculator.')
+    st.sidebar.info('This app is a demo for HLTC.')
