a01c74a00992686ab7d4f23aa2db0eb40c9bb429
Borungrujiphaisal <boonyachoke@hotmail.com>
2024-07-08 16:26:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.sidebar.header('Input Features')
 
 age                             = st.sidebar.number_input('Age', min_value=18, max_value=100, value=30)
 DSR                             = st.sidebar.number_input('Debt Service Ratio', min_value=0.0, value=0.3, step=0.01)
-ME                              = st.sidebar.number_input('Age', min_value=18, max_value=100, value=30)
+ME                              = st.sidebar.number_input('Monthly Expense', min_value=0, max_value=100000, value=15000, step=2500)
 ND                              = st.sidebar.number_input('Number of Dependents', min_value=0, max_value=10, value=3)
 WE                              = st.sidebar.number_input('Working experince (Months)', min_value=0, max_value=720, value=180)
 predict_button                  = st.sidebar.button('Predict')
